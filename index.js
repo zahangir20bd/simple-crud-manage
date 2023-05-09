ffe749a5b09729241167a14b06ac9db919a0c9b9
Zahangir Alam <eng.zahangir20bd@gmail.com>
2023-05-10 01:54:05 +0600

    post, delete method Added

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const cors = require("cors");
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const port = process.env.PORT || 5000;
 
 const app = express();
+
+const users = [{ id: 1, name: "Pronoy", email: "pronoy@mail.com" }];
 
 // Middleware
 app.use(cors());
@@ -28,6 +30,37 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    const userCollection = client.db("usersDB").collection("users");
+
+    // const database = client.db("usersDB");
+    // const userCollection = database.collection("users");
+
+    app.get("/", (req, res) => {
+      res.send("Simple Crud Server is Running");
+    });
+
+    app.get("/users", async (req, res) => {
+      const cursor = userCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+
+    app.post("/users", async (req, res) => {
+      const user = req.body;
+      console.log("new user:", user);
+      const result = await userCollection.insertOne(user);
+      res.send(result);
+    });
+
+    app.delete("/users/:id", async (req, res) => {
+      const id = req.params.id;
+      console.log("Please delete from database", id);
+      const query = { _id: new ObjectId(id) };
+      const result = await userCollection.deleteOne(query);
+      res.send(result);
+    });
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log(
@@ -35,14 +68,10 @@ async function run() {
     );
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
-
-app.get("/", (req, res) => {
-  res.send("Simple Crud Server is Running");
-});
 
 app.listen(port, () => {
   console.log(`Simple Crud Server running on port: ${port}`);
